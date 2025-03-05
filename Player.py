c3dcc84acdd94b22b6c0b184661c1d56dd4d6646
Jay Clock <146383205+clocktown123@users.noreply.github.com>
2025-03-05 15:38:12 -0700

    Update Player.py

--- Player.py
+++ Player.py
@@ -59,24 +59,24 @@ class player:
 
 
 
-    # def collision(self, map):
+    def collision(self, map):
 
-    #     if map[int(self.y / 50)][int((self.x + 30) / 50)] != 2:  # Check the right side of the player
-    #         self.x += 3  # Move right if there's no block
+        if map[int(self.y / 50)][int((self.x + 30) / 50)] != 2:  # Check the right side of the player
+            self.x += 3  # Move right if there's no block
         
 
-    #     # LEFT
-    #     if map[int(self.y / 50)][int(self.x / 50)] != 2:  # Check the left side of the player
-    #         self.x -= 3  # Move left if there's no block
+        # LEFT
+        if map[int(self.y / 50)][int(self.x / 50)] != 2:  # Check the left side of the player
+            self.x -= 3  # Move left if there's no block
 
-    #     # DOWN
-    #     if map[int((self.y + 30) / 50)][int(self.x / 50)] != 2:  # Check the bottom side of the player
-    #         self.y += 3  # Move down if there's no block
+        # DOWN
+        if map[int((self.y + 30) / 50)][int(self.x / 50)] != 2:  # Check the bottom side of the player
+            self.y += 3  # Move down if there's no block
 
-    #     # UP
-    #     if map[int(self.y / 50)][int(self.x / 50)] != 2:  # Check the top side of the player
-    #         self.y -= 3  # Move up if there's no block
+        # UP
+        if map[int(self.y / 50)][int(self.x / 50)] != 2:  # Check the top side of the player
+            self.y -= 3  # Move up if there's no block
         
-    #     else:
-    #         self.y += 0
-    #         self.x += 0
+        else:
+            self.y += 0
+            self.x += 0
