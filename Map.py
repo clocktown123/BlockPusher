dee1b8d7bea3881434fa4565bc431bb495d3e706
Jay Clock <146383205+clocktown123@users.noreply.github.com>
2025-03-10 22:30:53 -0600

    Update Map.py

--- Map.py
+++ Map.py
@@ -2,6 +2,7 @@ import pygame
 
 brick = pygame.image.load('DBrick.png')
 wall = pygame.image.load('Wall.png')
+Door = pygame.image.load('door.png')
 
 def MapF (screen, map):
     for i in range(12):
@@ -10,3 +11,5 @@ def MapF (screen, map):
                         screen.blit(brick, (j*50, i * 50), (0, 0, 50, 50))
                     if map[i][j] == 2:
                         screen.blit(wall, (j *50, i * 50), (0, 0, 50, 50))
+                    if map[i][j] == 3:
+                        screen.blit(Door, (j *50, i * 50), (0, 0, 50, 50))
