1d62718c5020551add1a78ff375d1bfa94121efd
Jay Clock <146383205+clocktown123@users.noreply.github.com>
2025-03-18 23:29:54 -0600

    Update Block.py

--- Block.py
+++ Block.py
@@ -8,6 +8,12 @@ class block:
         self.yPos = y
         self.vx = 0
         self.vy = 0
+        self.right = True
+        self.left = True
+        self.up = True
+        self.down = True
+        self.walk = False
+
 
     def draw(self, screen):
         screen.blit(Pblock, (self.xPos, self.yPos))
@@ -17,46 +23,86 @@ class block:
         self.xPos += self.vx
         self.yPos += self.vy
 
+    def Pcollision(self, p1):
+        if p1.x + 50 > self.xPos and p1.x<self.xPos+50 and p1.y +50 > self.yPos and p1.y < self.yPos+50 and self.walk == False:
+                if p1.direction == 2 and self.up == True:
+                    self.yPos -= 50
+                elif p1.direction == 2 and self.up == False:
+                    print("down")
+                    p1.y = self.yPos+50
+
+                if p1.direction == 3 and self.down == True:
+                    self.yPos += 50
+                elif p1.direction == 3 and self.down == False:
+                    print("up")
+                    p1.y = self.yPos - 50
+                    
+                if p1.direction == 1 and self.left == True:
+                    self.xPos -= 50
+                elif p1.direction == 1 and self.left == False:
+                    p1.x = self.xPos + 50
+                    print("left")
+
+                if p1.direction == 0 and self.right == True:
+                    self.xPos += 50
+                elif p1.direction == 0 and self.right == False:
+                    print("right")
+                    p1.x = self.xPos - 50
+
+
     def Bcollision(self, map):
-
-        if map[int(self.yPos / 50)][int((self.xPos + 48) / 50)] != 2:  # Check the right side of the player
-            self.xPos += 3  # Move right if there's no block
         
-        # LEFT
-        if map[int(self.yPos / 50)][int(self.xPos / 50)] != 2:  # Check the left side of the player
-            self.xPos -= 3  # Move left if there's no block
+        if map[int(self.yPos / 50)][int((self.xPos + 50) / 50)] == 2:  # Check the right side of the player
+            self.right = False
+        else: 
+            self.right = True
+        
 
-        # DOWN
-        if map[int((self.yPos + 48) / 50)][int(self.xPos / 50)] != 2:  # Check the bottom side of the player
-            self.yPos += 3  # Move down if there's no block
+        # LEFT
+        if map[int(self.yPos / 50)][int((self.xPos - 50) / 50)] == 2:  # Check the left side of the player
+            self.left = False
+        else:
+            self.left = True
+
+        #DOWN
+        if map[int((self.yPos + 50) / 50)][int(self.xPos / 50)] == 2:  # Check the bottom side of the player
+            self.down = False
+        else:
+            self.down = True
 
         # UP
-        if map[int(self.yPos / 50)][int(self.xPos / 50)] != 2:  # Check the top side of the player
-            self.yPos -= 3  # Move up if there's no block
-            
+        if map[int((self.yPos - 50) / 50)][int(self.xPos / 50)] == 2:  # Check the top side of the player
+            self.up = False
         else:
-            self.yPos += 0
-            self.xPos += 0
+            self.up = True
+        
+
+            
+        
 
     def NLCollision(self, map):
-        if map[int(self.yPos / 50)][int((self.xPos + 50) / 50)] == 3 or map[int(self.yPos / 50)][int(self.xPos / 50)] == 3 or map[int((self.yPos + 50) / 50)][int(self.xPos / 50)] == 3 or map[int(self.yPos / 50)][int(self.xPos / 50)] == 3:
+        #if map[int(self.yPos / 50)][int((self.xPos + 50) / 50)] == 3 or map[int(self.yPos / 50)][int((self.xPos - 50)/ 50)] == 3 or map[int((self.yPos + 50) / 50)][int(self.xPos / 50)] == 3 or map[int((self.yPos - 50) / 50)][int(self.xPos / 50)] == 3:
                  # Check and assign x and y position based on where the "3" block is
-            if map[int(self.yPos / 50)][int((self.xPos + 50) / 50)] == 3:
-                self.xPos = int((self.xPos + 50) / 50) * 50  # Set xPos to the column of the "3" block
-                self.yPos = int(self.yPos / 50) * 50  # Set yPos to the row of the "3" block
-                return True
+
+        if map[int(self.yPos / 50)][int((self.xPos + 50) / 50)] == 4:
+            self.walk = True
+
+        elif map[int(self.yPos / 50)][int(self.xPos/ 50)] == 3:
+            self.walk = True
+            return True
             
-            elif map[int(self.yPos / 50)][int(self.xPos / 50)] == 3:
-                self.xPos = int(self.xPos / 50) * 50  # Set xPos to the column of the "3" block
-                self.yPos = int(self.yPos / 50) * 50  # Set yPos to the row of the "3" block
-                return True
+        # elif map[int(self.yPos / 50)][int((self.xPos - 50) / 50)] == 3:
+        #     self.walk = True
+        #     return True
 
-            elif map[int((self.yPos + 50) / 50)][int(self.xPos / 50)] == 3:
-                self.xPos = int(self.xPos / 50) * 50  # Set xPos to the column of the "3" block
-                self.yPos = int((self.yPos + 50) / 50) * 50  # Set yPos to the row of the "3" block
-                return True
+        # elif map[int((self.yPos + 50) / 50)][int(self.xPos / 50)] == 3:
+        #     self.walk = True
+        #     return True
 
-            elif map[int(self.yPos / 50)][int(self.xPos / 50)] == 3:
-                self.xPos = int(self.xPos / 50) * 50  # Set xPos to the column of the "3" block
-                self.yPos = int(self.yPos / 50) * 50  # Set yPos to the row of the "3" block
-                return True
+        # elif map[int((self.yPos - 50) / 50)][int(self.xPos / 50)] == 3:
+        #     self.walk = True
+        #     return True
+            
+        else:
+            self.walk = False
+            return False
