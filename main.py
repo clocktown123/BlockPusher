556da410100f83764c56d47438fba0e4d0b7de69
Jay Clock <146383205+clocktown123@users.noreply.github.com>
2025-03-18 23:31:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from Player import player
 from Block import block
 
 pygame.init()
-pygame.display.set_caption("top down grid map game")
+pygame.display.set_caption("BlockPusher")
 screen = pygame.display.set_mode((600,600))
 clock = pygame.time.Clock()
 
 p1 = player()
+b1 = [block(400, 400),block(400, 300)]
 
 
 A = 0
@@ -33,6 +34,8 @@ mapNum = 1
 nextLVL = False
 nextLVLs2 = False
 
+canMove = [True, True, True, True]
+
 map = [[2,2,2,2,2,2,2,2,2,2,2,2],
        [2,1,1,1,1,1,1,1,1,1,1,2],
        [2,1,1,1,1,1,1,1,1,3,1,2],
@@ -51,13 +54,15 @@ map2 = [[2,2,2,2,2,2,2,2,2,2,2,2],
        [2,1,1,1,1,1,1,1,1,1,1,2],
        [2,1,1,1,1,1,1,1,1,1,1,2],
        [2,1,1,1,1,1,1,1,1,1,1,2],
-       [2,1,1,1,1,1,1,1,1,1,1,2],
+       [2,4,4,4,4,4,4,4,4,4,4,2],
        [2,1,1,1,1,1,1,1,1,1,1,2],
        [2,1,1,1,1,1,1,1,1,1,1,2],
        [2,1,1,1,1,1,1,1,1,1,1,2],
        [2,1,1,1,1,1,1,1,1,1,1,2],
        [2,1,1,1,1,1,1,1,1,1,1,2],
        [2,2,2,2,2,2,2,2,2,2,2,2]]
+
+blockBag = []
 
 
 text_font = pygame.font.SysFont("Sans", 30, bold = True)
@@ -66,11 +71,6 @@ def draw_text(text, font, text_col, tx, ty):
     img = font.render(text, True, text_col)
     screen.blit(img, (tx, ty))
 
-numBlocks = 2
-
-blockBag = []
-for i in range(numBlocks):
-    blockBag.append(block(random.randrange(300, 450), random.randrange(150, 450)))
 
 while True: #GAME LOOP######################################################
     clock.tick(60) # fps
@@ -79,8 +79,6 @@ while True: #GAME LOOP######################################################
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameover = True
-
-
     if event.type == pygame.KEYDOWN:
              
         if event.key == pygame.K_RIGHT:
@@ -100,86 +98,56 @@ while True: #GAME LOOP######################################################
 
         if event.key == pygame.K_RIGHT:
             keys[A] = False
-                #RowNum = 0
+            p1.moves[0] = True
         if event.key == pygame.K_LEFT:
             keys[D] = False
-                #RowNum = 3
+            p1.moves[1] = True
         if event.key == pygame.K_UP:
             keys[W] = False
-                #RowNum = 1
+            p1.moves[2] = True
         if event.key == pygame.K_DOWN:
             keys[S] = False
-                #RowNum = 2
+            p1.moves[3] = True
 
     #physics section----------------------------------------------------------
 
-    # p1.collision(map)
-    for h in range(len(blockBag)):
-        p1.BlockCollision(blockBag[h].xPos, blockBag[h].yPos)
-        p1.BlockCollision(blockBag[h].xPos, blockBag[h].yPos)
     if mapNum == 1:
         p1.collision(map)
-        for g in blockBag:
+        for g in b1:
             g.Bcollision(map)
     elif mapNum == 2:
         p1.collision(map2)
-        for g in blockBag:
+        for g in b1:
             g.Bcollision(map2)
 
 
     for i in blockBag:
         i.move()
 
+    
+    
+    #print(canMove[0])
     functs = []
 
     if mapNum == 1:
-        for p in range(len(blockBag)):
-            blockBag[p].NLCollision(map)
-            functs.append(blockBag[p].NLCollision(map))
+        for m in b1:
+            m.NLCollision(map)
+            functs.append(m.NLCollision(map))
             if all(functs):
                 nextLVL = True
+    if mapNum == 2:
+         for m in b1:
+            m.NLCollision(map2)
+            # functs.append(m.NLCollision(map))
+            # if all(functs):
+            #     nextLVL = True
 
-    for m in blockBag:
-        if p1.x + 30 > m.xPos and p1.x < m.xPos + 50:
-                if p1.y + 30 > m.yPos and p1.y < m.yPos:
-                    # Player is touching the box from below7
-                    m.yPos += 2  # Move the box down
-                    p1.y = m.yPos - 30
-                elif p1.y < m.yPos +50 and p1.y + 30 > m.yPos + 50:
-                    # Player is touching the box from above
-                    m.yPos -= 2  # Move the box up
-                    p1.y = m.yPos + 50
+    #print(p1.direction)
+    for m in b1:
+        m.Pcollision(p1)
 
-        if p1.y + 30 > m.yPos and p1.y < m.yPos + 50:
-            if p1.x + 30 > m.xPos and p1.x < m.xPos:
-                # Player is touching the box from the right
-                m.xPos += 2  # Move the box right
-                p1.x = m.xPos - 30
-            elif p1.x < m.xPos + 50 and p1.x + 30 > m.xPos + 50:
-                # Player is touching the box from the left
-                m.xPos -= 2  # Move the box left
-                p1.x = m.xPos + 50
-
-
-    # if p1.x + 30 > b2.xPos and p1.x < b2.xPos + 50:
-    #         if p1.y + 30 > b2.yPos and p1.y < b2.yPos:
-    #             # Player is touching the box from below
-    #             b2.yPos += 2  # Move the box down
-    #             p1.y = b2.yPos - 30
-    #         elif p1.y < b2.yPos +50 and p1.y + 30 > b2.yPos + 50:
-    #             # Player is touching the box from above
-    #             b2.yPos -= 2  # Move the box up
-    #             p1.y = b2.yPos + 50
-
-    # if p1.y + 30 > b2.yPos and p1.y < b2.yPos + 50:
-    #     if p1.x + 30 > b2.xPos and p1.x < b2.xPos:
-    #         # Player is touching the box from the right
-    #         b2.xPos += 2  # Move the box right
-    #         p1.x = b2.xPos - 30
-    #     elif p1.x < b2.xPos + 50 and p1.x + 30 > b2.xPos + 50:
-    #         # Player is touching the box from the left
-    #         b2.xPos -= 2  # Move the box left
-    #         p1.x = b2.xPos + 50
+                
+    print(p1.direction)
     
 
     if nextLVL == True :
@@ -195,15 +163,17 @@ while True: #GAME LOOP######################################################
     screen.fill((230,100,100))# Clear the screen pink
 
     if mapNum == 1:
-        MapF(screen, map)
+        MapF(screen, map, blockBag)
         
     if mapNum == 2:
-        MapF(screen, map2)
+        MapF(screen, map2, blockBag)
+
+    for i in range(len(b1)):
+        b1[i].draw(screen)
 
     p1.draw(screen)
-
-    for k in blockBag:
-        k.draw(screen)
+    
+        
 
     pygame.display.flip()#this actually puts the pixel on the screen
 
