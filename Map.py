c6baf493cb8463bff7acad765a34aacf77f88a2c
Jay Clock <146383205+clocktown123@users.noreply.github.com>
2025-04-08 00:45:05 -0600

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,10 +1,14 @@
 import pygame
 from Block import block
 
-brick = pygame.image.load('DBrick.png')
+Ground = pygame.image.load('Ground.png')
+Ground1 = pygame.image.load('Ground1.png')
+
+Lm = pygame.image.load('LandMine.png')
+
 wall = pygame.image.load('Wall.png')
 Door = pygame.image.load('door.png')
-lava = pygame.image.load("Lava.png")
+water = pygame.image.load("Water2.png")
 holding = pygame.image.load("Holder.png")
 Button = pygame.image.load("button.png")
 glass = pygame.image.load("Glass.png")
@@ -13,14 +17,19 @@ glass = pygame.image.load("Glass.png")
 def MapF(screen, map, blockBeg):
     for i in range(12):
         for j in range(12):
+            #ground-------------------------------
             if map[i][j] == 1:
-                screen.blit(brick, (j * 50, i * 50), (0, 0, 50, 50))
+                screen.blit(Ground, (j * 50, i * 50), (0, 0, 50, 50))
+            elif map[i][j] == 8:
+                screen.blit(Ground1, (j * 50, i * 50), (0, 0, 50, 50))
+
+            
             elif map[i][j] == 2:
                 screen.blit(wall, (j * 50, i * 50), (0, 0, 50, 50))
             elif map[i][j] == 3:
                 screen.blit(Door, (j * 50, i * 50), (0, 0, 50, 50))
             elif map[i][j] == 4:
-                screen.blit(lava, (j * 50, i * 50), (0, 0, 50, 50))
+                screen.blit(Lm, (j * 50, i * 50), (0, 0, 50, 50))
             elif map[i][j] == 5:
                 screen.blit(holding, (j * 50, i * 50), (0, 0, 50, 50))
             elif map[i][j] == 6:
