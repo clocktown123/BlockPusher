cb2ee7bcf5663a777e96d3317e031de77754ea9f
Jay Clock <146383205+clocktown123@users.noreply.github.com>
2025-04-08 21:59:28 -0600

    Update Player.py

--- Player.py
+++ Player.py
@@ -5,6 +5,9 @@ D = 1
 W = 2
 S = 3
 
+cowboy = pygame.image.load('CowboySprite.png') #load your spritesheet
+cowboy.set_colorkey((255, 0, 255)) #this makes bright pink (255, 0, 255) transparent (sort of)
+
 class player:
     def __init__(self):
         self.x = 300
@@ -19,8 +22,13 @@ class player:
         self.down = True
         self.isAlive = True
 
+        self.frameWidth = 50
+        self.frameHeight = 50
+        self.RowNum = 2 #for left animation, this will need to change for other animations
+        self.frameNum = 0
+
     def draw(self, screen):
-        pygame.draw.rect(screen, (150, 100, 100), (self.x, self.y, 50, 50))
+        screen.blit(cowboy, (self.x, self.y), (self.frameWidth*self.frameNum, self.RowNum*self.frameHeight, self.frameWidth, self.frameHeight)) 
     def move(self, keys, map):
         #print("true 2")
         #LEFT MOVEMENT
@@ -28,11 +36,19 @@ class player:
             self.vx = 50
             self.direction = A
             self.moves[0] = False
+            self.RowNum = 2
+            self.frameNum += 1
+            if self.frameNum > 3:
+                self.frameNum = 0
         #RIGHT MOVEMENT
         elif keys[D] == True and self.moves[1] == True and self.left == True:
             self.vx = -50
             self.direction = D
             self.moves[1] = False
+            self.RowNum = 1
+            self.frameNum += 1
+            if self.frameNum > 3:
+                self.frameNum = 0
         #TURN OFF X VELOCITY
         else:
             self.vx = 0
@@ -41,10 +57,18 @@ class player:
             self.vy = -50
             self.direction = W
             self.moves[2] = False
+            self.RowNum = 3
+            self.frameNum += 1
+            if self.frameNum > 3:
+                self.frameNum = 0
         elif keys[S] == True and self.moves[3] == True and self.down == True:
             self.vy = 50
             self.direction = S
             self.moves[3] = False
+            self.RowNum = 0
+            self.frameNum += 1
+            if self.frameNum > 3:
+                self.frameNum = 0
         else:
             self.vy = 0
 
