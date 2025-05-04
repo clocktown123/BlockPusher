ea19c7d49e0440c2db0dfe09dee47ee9809e65d8
Jay Clock <146383205+clocktown123@users.noreply.github.com>
2025-05-04 02:06:18 -0600

    Update Player.py

--- Player.py
+++ Player.py
@@ -11,6 +11,9 @@ S = 3
 cowboy = pygame.image.load('CowboySprite.png') #load your spritesheet
 cowboy.set_colorkey((255, 0, 255)) #this makes bright pink (255, 0, 255) transparent (sort of)
 
+ExplosionSF = pygame.mixer.Sound("BlockPushExplosion.mp3")
+explode = pygame.image.load("Explosion.png")
+
 class player:
     def __init__(self):
         self.x = 300
@@ -29,6 +32,11 @@ class player:
         self.frameHeight = 50
         self.RowNum = 2 #for left animation, this will need to change for other animations
         self.frameNum = 0
+
+        self.showing_image = False
+        self.image_start_time = 0
+        self.image_stage = 0  # 0: nothing, 1: showing image1, 2: showing image2
+        self.image_played = False  # New flag to track if animation played while on tile 3
 
     def draw(self, screen):
         screen.blit(cowboy, (self.x, self.y), (self.frameWidth*self.frameNum, self.RowNum*self.frameHeight, self.frameWidth, self.frameHeight)) 
@@ -78,6 +86,32 @@ class player:
         self.x += self.vx
         self.y += self.vy
 
+    def ExplosionAni(self, screen, map, current_time):
+
+        # Only trigger once per entry into a '3' tile
+        if map[int(self.y / 50)][int(self.x/ 50)] == 4:
+            if self.image_played == False and self.showing_image == False:
+                self.showing_image = True
+                self.image_start_time = current_time
+                self.image_played = True
+        else:
+            # Reset trigger when leaving the tile
+            self.image_played = False
+            self.isAlive = True
+
+        # Handle image showing logic
+        if self.showing_image:
+            TimePassed = current_time - self.image_start_time
+
+            if TimePassed < 1000:
+                screen.blit(explode, (self.x, self.y))
+                pygame.mixer.Sound.play(ExplosionSF)
+                ExplosionSF.set_volume(0.04)
+            elif TimePassed > 1000:
+                self.isAlive = False
+                
+
+
 
 
     def collision(self, map):
@@ -106,10 +140,10 @@ class player:
         else:
             self.up = True
         
-        if map[int(self.y / 50)][int(self.x / 50)] == 4:  # Check the top side of the player
-            self.isAlive = False
-        else:
-            self.isAlive = True
+        # if map[int(self.y / 50)][int(self.x / 50)] == 4:  # Check the top side of the player
+        #     self.isAlive = False
+        # else:
+        #     self.isAlive = True
         # else:
         #     self.y += 0
         #     self.x += 0
